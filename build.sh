ab891b699b519f6719377a73feeaf9865061c735
Ethan Li <lietk12@gmail.com>
2024-09-30 21:36:12 -0700

    Remove opentabletdriver, add kde calendar system integration

--- build.sh
+++ build.sh
@@ -2,13 +2,7 @@
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E %fedora)"
-
-# prepare to install userspace tablet driver
-# note: the user needs to manually enable the systemctl service according to https://opentabletdriver.net/Wiki/FAQ/Linux#autostart
-wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm -O /tmp/opentabletdriver.rpm
-
 rpm-ostree install \
   acpica-tools \
   kio-fuse \
-  /tmp/opentabletdriver.rpm
+  merkuro kdepim-addons kdepim-runtime # I couldn't get these integrations to work via Flatpak
