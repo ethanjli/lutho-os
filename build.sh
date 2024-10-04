ff17f97ebbab5fa3dcc380fb1ad87a98ed69eaff
Ethan Li <lietk12@gmail.com>
2024-10-03 19:00:43 -0700

    Remove opentabletdriver, add kde calendar system integration (#119)

--- build.sh
+++ build.sh
@@ -2,14 +2,10 @@
 
 set -ouex pipefail
 
-# prepare to install userspace tablet driver
-# note: the user needs to manually enable the systemctl service according to https://opentabletdriver.net/Wiki/FAQ/Linux#autostart
-wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm -O /tmp/opentabletdriver.rpm
-
 rpm-ostree install \
   acpica-tools \
   kio-fuse \
-  /tmp/opentabletdriver.rpm
+  merkuro kdepim-addons kdepim-runtime # I couldn't get these integrations to work via Flatpak
 
 # install zerotier
 /tmp/install-zerotier.sh
