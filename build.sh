f4469680e54e5390ac1b698c05ad33579c935a8b
Ethan Li <lietk12@gmail.com>
2024-11-03 08:39:06 -0800

    Try to preserve installed udev rules for opentabletdriver

--- build.sh
+++ build.sh
@@ -5,6 +5,12 @@ set -ouex pipefail
 # prepare to install userspace tablet driver (this works, and I couldn't get the Flatpak to work standalone)
 # note: the user needs to manually enable the systemctl service according to https://opentabletdriver.net/Wiki/FAQ/Linux#autostart
 wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm -O /tmp/opentabletdriver.rpm
+for c in /etc/udev/rules.d/9{0,9}-opentabletdriver.rules; do
+  if [ -f "${c}" ]; then
+    echo "Copying ${c} into /usr"
+    cp "$c" "/usr/${c}"
+  fi
+done
 
 rpm-ostree install \
   acpica-tools \
