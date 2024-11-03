8dbc160b3b7e792bfd2ca8425c4db4a1973610e6
Ethan Li <lietk12@gmail.com>
2024-11-03 10:53:05 -0800

    Revert "Don't add OpenTabletDriver (udev rules are already available for Flatpak to use)"
    This reverts commit c56f968b64ddb855e10a5429c73cc61ac8b07e16.

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@
 
 set -ouex pipefail
 
+# prepare to install userspace tablet driver (this works, and I couldn't get the Flatpak to work standalone)
+# note: the user needs to manually enable the systemctl service according to https://opentabletdriver.net/Wiki/FAQ/Linux#autostart
+wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm -O /tmp/opentabletdriver.rpm
+
 rpm-ostree install \
   acpica-tools \
   kio-fuse \
+  /tmp/opentabletdriver.rpm \
   merkuro kdepim-addons kdepim-runtime # I couldn't get these integrations to work via Flatpak
 
 # install zerotier
