b63bfff4107c4fa5c167b23de559e2fcb969327e
Ethan Li <lietk12@gmail.com>
2024-11-03 08:55:17 -0800

    Fix incorrect ordering for udev rule copying

--- build.sh
+++ build.sh
@@ -5,18 +5,21 @@ set -ouex pipefail
 # prepare to install userspace tablet driver (this works, and I couldn't get the Flatpak to work standalone)
 # note: the user needs to manually enable the systemctl service according to https://opentabletdriver.net/Wiki/FAQ/Linux#autostart
 wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm -O /tmp/opentabletdriver.rpm
-for c in /etc/udev/rules.d/*-opentabletdriver.rules; do
-  if [ -f "${c}" ]; then
-    echo "Copying ${c} into /usr"
-    cp "$c" "/usr/${c}"
-  fi
-done
 
 rpm-ostree install \
   acpica-tools \
   kio-fuse \
   /tmp/opentabletdriver.rpm \
   merkuro kdepim-addons kdepim-runtime # I couldn't get these integrations to work via Flatpak
+
+# set up udev rules for userspace tablet driver
+ls /etc/udev/rules.d
+for c in /etc/udev/rules.d/*-opentabletdriver.rules; do
+  if [ -f "${c}" ]; then
+    echo "Copying ${c} into /usr"
+    cp "$c" "/usr/${c}"
+  fi
+done
 
 # install zerotier
 /tmp/install-zerotier.sh
