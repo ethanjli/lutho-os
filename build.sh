b4f0bac9e1c55fd61e154807be16ba50320d9afc
Ethan Li <lietk12@gmail.com>
2024-11-03 08:46:19 -0800

    Try to fix udev rule file check

--- build.sh
+++ build.sh
@@ -6,10 +6,8 @@ set -ouex pipefail
 # note: the user needs to manually enable the systemctl service according to https://opentabletdriver.net/Wiki/FAQ/Linux#autostart
 wget https://github.com/OpenTabletDriver/OpenTabletDriver/releases/latest/download/OpenTabletDriver.rpm -O /tmp/opentabletdriver.rpm
 for c in /etc/udev/rules.d/9{0,9}-opentabletdriver.rules; do
-  if [ -f "${c}" ]; then
-    echo "Copying ${c} into /usr"
-    cp "$c" "/usr/${c}"
-  fi
+  echo "Copying ${c} into /usr"
+  cp "$c" "/usr/${c}"
 done
 
 rpm-ostree install \
